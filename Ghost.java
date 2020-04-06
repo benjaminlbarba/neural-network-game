c338ee561aeb4b3d7dc0c6aa9dbb56217c887676
quyufei500 <63222393+quyufei500@users.noreply.github.com>
2020-04-06 09:17:01 -0400

    Added Directions enum and populates all three characters class with methods and fields

--- Ghost.java
+++ Ghost.java
@@ -1,6 +1,16 @@
 /**
- * Ghost is the class inherited from BasicCharacter class that contains all relevant fields and methods related to ghosts moving on the map. 
+ * Ghost is the class inherited from BasicCharacter class that contains all relevant 
+ * fields and methods related to ghosts moving on the map. The only specification that 
+ * needs to make in Ghost class against BasicCharacter class is to implement the abstract
+ * method update();
  */
 public class Ghost extends BasicCharacter {
-
+	/**
+	 * Implemented from the abstract method in BasicCharacter class,
+	 * update method is called every frame of the game by the governing update method in Game class.
+	 * it updates the positions (x, y) and directions (dir) of the Ghost without needing to process
+	 * any keyboard input. 
+	 */
+	public void update() {
+	}
 }
--- Pacman.java
+++ Pacman.java
@@ -1,6 +1,15 @@
 /**
- * Pacman is the class inherited from BasicCharacter class that contains all relevant fields and methods related to the pacman moving on the map.
+ * Pacman is the class inherited from BasicCharacter class that contains all relevant 
+ * fields and methods related to the pacman moving on the map. The only specification that 
+ * needs to make in Pacman class against BasicCharacter class is to implement the abstract
+ * method update();
  */
 public class Pacman extends BasicCharacter{
-
+	/**
+	 * Implemented from the abstract method in BasicCharacter class,
+	 * update method is called every frame of the game by the governing update method in Game class.
+	 * it updates the positions (x, y) and directions (dir) of the pacman with processed key inputs.
+	 */
+	public void update() {
+	}
 }
