16c78573230d51446d9c5e0c77d77289c310a901
dsfwef <53978912+sxw1@users.noreply.github.com>
2020-04-07 00:33:45 -0700

    Update Pacman.java
    update Pacman class

--- Pacman.java
+++ Pacman.java
@@ -5,17 +5,113 @@
  * method update();
  */
 public class Pacman extends BasicCharacter{
+
+	public int stepSize = 0; // must be factor of blockSize (length of row and col)
+	private Map map;
+	private boolean isAddScore;
+	private Directions nextDir;
+
 	
-	public Pacman(int x, int y, Directions dir, String charImageLink) {
+	public Pacman(int x, int y, Directions dir, String charImageLink, Map m) {
 		super(x, y, dir, charImageLink);
-		// TODO Auto-generated constructor stub
+		map = m;
+		nextDir = Directions.STILL;
 	}
 
 	/**
 	 * Implemented from the abstract method in BasicCharacter class,
 	 * update method is called every frame of the game by the governing update method in Game class.
 	 * it updates the positions (x, y) and directions (dir) of the pacman with processed key inputs.
+	 * Update score based on whether location has dot.
 	 */
 	public void update() {
+		// TODO
+
+		// Update position
+		if (dir != Directions.STILL) {
+			if(dir == Directions.UP) {
+				y -= stepSize;
+			}
+			else if (dir == Directions.DOWN) {
+				y += stepSize;
+			}
+			else {
+				x = dir == Directions.LEFT ? x - stepSize : x + stepSize;
+			}
+		}
+	}
+
+	/**
+	 * Return whether next position is accessible given dir.
+	 * @param dir direction for next position
+	 * @see Directions
+	 * @return boolean whether next position is accessible given dir.
+	 */
+	private boolean dirMovable(Directions dir) {
+		// TODO. Merge to BasicCharacter
+		return false;
+	}
+
+	/**
+	 * Getter for isAddScore
+	 * @return boolean whether score should be incremented if dot is eaten.
+	 */
+	public boolean isAddScore() {
+		return isAddScore;
+	}
+
+	/**
+	 * Set isAddScore to true;
+	 */
+	private void addScore() {
+		isAddScore = true;
+	}
+
+	/**
+	 * Getter for x.
+	 * @return x
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * Getter for y.
+	 * @return y
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * Getter for direction.
+	 * @return dir
+	 */
+	public Directions getDir() {
+		return dir;
+	}
+
+	/**
+	 * Setter for direction.
+	 * @param d direction to be set.
+	 */
+	public void setDir(Directions d) {
+		dir = d;
+	}
+
+	/**
+	 * Getter for nextDir.
+	 * @return nextDir
+	 */
+	public Directions getNextDir() {
+		return nextDir;
+	}
+
+	/**
+	 * Setter for nextDir
+	 * @param nextDir nextDir to be set
+	 */
+	public void setNextDir(Directions nextDir) {
+		this.nextDir = nextDir;
 	}
 }
